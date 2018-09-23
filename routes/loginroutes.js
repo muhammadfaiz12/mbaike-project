1cb5b330d71597c1bd839ea9584f06241a9e1e5f
muhammad.faiz <faiz00.muhammad@gmail.com>
2018-09-23 10:17:00 +0700

    init commit

---
+++ routes/loginroutes.js
@@ -0,0 +1,47 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'kicut',
+  password : '',
+  database : 'cloudprint'
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
+
+exports.login = function(req,res){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+  if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    // console.log('The solution is: ', results);
+    if(results.length >0){
+      if(results[0].password == password){
+        res.redirect('/homepage');;
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email and password does not match"
+            });
+      }
+    }
+    else{
+      res.send({
+        "code":204,
+        "success":"Email does not exits"
+          });
+    }
+  }
+  });
+}
